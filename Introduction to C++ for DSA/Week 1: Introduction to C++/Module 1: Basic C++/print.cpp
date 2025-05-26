94adb58edbaeea00a54ab9ab6121da8d1fcefd88
GeorgeBlaize <92225843+GeorgeBlaize@users.noreply.github.com>
2025-05-26 20:54:11 +0600

    Add files via upload

---
+++ Introduction to C++ for DSA/Week 1: Introduction to C++/Module 1: Basic C++/print.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+int main()
+{
+    int x=100;
+    char c='A';
+    double d=34.56;
+
+    std::cout<<x << " " << c <<std::endl<< d <<std::endl;
+
+    printf("My favourite number is %d",x);
+    std:: cout<<"My favourite number is "<< x << std::endl;
+
+
+
+    return 0;
+}
